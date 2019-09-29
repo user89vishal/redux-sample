0d8bc4254af2e23dd0ae8df31668016c778775e9
user89vishal <vishal.srivastava1@3pillarglobal.com>
2019-09-29 10:19:05 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./actions";
 
 function App() {
+  const a = 5;
   const counter = useSelector(state => state.counter);
   const dispatch = useDispatch();
 
